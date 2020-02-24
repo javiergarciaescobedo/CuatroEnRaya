67c8e3d440a0f88ce4df06af203d8c670fb82606
Javier García Escobedo <javiergarbedo@gmail.com>
2020-02-24 09:26:21 +0100

    Caída de la ficha

--- src/main/java/es/javiergarciaescobedo/cuatroenraya/Tablero.java
+++ src/main/java/es/javiergarciaescobedo/cuatroenraya/Tablero.java
@@ -1,11 +1,16 @@
 package es.javiergarciaescobedo.cuatroenraya;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Line;
+import javafx.util.Duration;
 
 public class Tablero extends Pane {
-        
+                    
     public Tablero() {
         for(int i=0; i<8; i++) {
             Line line = new Line(Ficha.TAM_FICHA*i, Ficha.TAM_FICHA, 
@@ -28,6 +33,21 @@ public class Tablero extends Pane {
         ficha.setLayoutX((columna + 0.5) * Ficha.TAM_FICHA);
         ficha.setLayoutY(Ficha.TAM_FICHA / 2);
         this.getChildren().add(ficha);
+        
+        caerFicha(ficha);
+    }
+    
+    private void caerFicha(Ficha ficha) {
+        Timeline timeline = new Timeline(
+            new KeyFrame(Duration.seconds(0.017), (ActionEvent ae) -> {
+                ficha.setLayoutY(ficha.getLayoutY() + 1);
+            })
+        );
+        timeline.setCycleCount(Ficha.TAM_FICHA * 6);
+        timeline.play();
+        timeline.setOnFinished((ActionEvent ae) -> {
+            System.out.println("Ficha colocada en su sitio");
+        });
     }
     
 }
